5abcaea87e2219df85bb2b1371efe3b386dcd548
Chris Galbraith <chris@resknow.co.uk>
2019-11-27 16:16:20 +0000

    Refactor plugin.php

---
+++ classes/Plugin.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Form;
+
+use Exception;
+
+class Plugin {
+
+    protected $pluginDir; // Form plugin directory
+
+    public function __construct() {
+        plugin_requires_version( 'form', '4.0.0-dev' );
+
+        // Make sure Dependencies are installed
+        if ( !class_exists('\\GUMP') || !class_exists('\\Medoo\\Medoo') || !class_exists('\\PHPMailer\\PHPMailer\\PHPMailer') ) {
+            throw new Exception('Run `composer require wixel/gump:1.5.7 phpmailer/phpmailer:6.0 catfan/medoo:1.4` in your terminal to finish setting up Form');
+        }
+
+        $this->pluginDir = plugin_dir() . '/form';
+
+        add_action('init', [$this, 'init']);
+        add_action('template.init', [$this, 'pathSetup']);
+    }
+
+    /**
+     * Init
+     * 
+     * Loads configured forms and checks site config
+     */
+    public function init() {
+    
+        // Filter: form.load
+        set( 'site.forms', apply_filters( 'form.load', get('site.forms') ) );
+    
+        // Make sure we have some config to work with
+        if ( !get('site.forms') ) {
+            return;
+        }
+
+        if ( !get('site.email') ) {
+            throw new Exception( 'You must set the "email" field in your config for the Form plugin to function.' );
+        }
+
+    }
+
+    /**
+     * Path Setup
+     * 
+     * Sets up form routes for validation and submission
+     */
+    public function pathSetup() {
+
+        do_action( 'form.init' );
+
+        // Validation
+        if ( path_contains('validate') ) {
+            require_once $this->pluginDir . '/inc/validate.php';
+        }
+    
+        // Submission
+        if ( !path_contains('validate') && path_contains('submit') || isset( $_POST['bp-form-id'] ) ) {
+            require_once $this->pluginDir . '/inc/submit.php';
+        }
+    
+        // API
+        require_once $this->pluginDir . '/inc/api.php';
+
+    }
+
+}
--- plugin.php
+++ plugin.php
@@ -2,6 +2,7 @@
 
 use Form\Form;
 use Form\JSON;
+use Form\Plugin;
 
 /**
  * @subpackage Form
@@ -10,32 +11,11 @@ use Form\JSON;
  * @author Chris Galbraith
  */
 
-// Set Minimum Boilerplate Version
-plugin_requires_version( 'form', '4.0.0-dev' );
-
-// Make sure Dependencies are installed
-if ( !class_exists('\\GUMP') || !class_exists('\\Medoo\\Medoo') || !class_exists('\\PHPMailer\\PHPMailer\\PHPMailer') ) {
-    throw new Exception('Run `composer require wixel/gump:1.5.7 phpmailer/phpmailer:6.0 catfan/medoo:1.4` in your terminal to finish setting up Form');
-}
+require_once __DIR__ . '/classes/Plugin.php';
+new Plugin();
 
 // Add core filters
 require_once __DIR__ . '/inc/filters.php';
-
-add_action( 'init', function() {
-
-    if ( !get('site.email') ) {
-        throw new Exception( 'You must set the "email" field in your config for the Form plugin to function.' );
-    }
-
-    // Filter: form.load
-    set( 'site.forms', apply_filters( 'form.load', get('site.forms') ) );
-
-    // Make sure we have some config to work with
-    if ( !get('site.forms') ) {
-        return;
-    }
-
-} );
 
 // Load Assets
 require_once __DIR__ . '/inc/assets.php';
@@ -50,23 +30,3 @@ require_once __DIR__ . '/inc/functions.php';
 
 // Include Render functions
 require_once __DIR__ . '/inc/render.php';
-
-// Init
-add_action( 'template.init', function() use ($_theme, $_path) {
-
-    do_action( 'form.init' );
-
-    // Validation
-    if ( path_contains('validate') ) {
-        require_once __DIR__ . '/inc/validate.php';
-    }
-
-    // Submission
-    if ( !path_contains('validate') && path_contains('submit') || isset( $_POST['bp-form-id'] ) ) {
-        require_once __DIR__ . '/inc/submit.php';
-    }
-
-    // API
-    require_once __DIR__ . '/inc/api.php';
-
-} );
